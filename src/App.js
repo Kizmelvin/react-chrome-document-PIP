a11f9f590354985f811b7da7d52057bdc9947e94
Kizmelvin <chidiebere.johnbosco@gmail.com>
2024-04-03 15:56:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useRef } from "react";
+// import usePictureInPicture from "react-use-pip";
+
+// function App() {
+//   const videoRef = useRef(null);
+//   const {
+//     isPictureInPictureActive,
+//     isPictureInPictureAvailable,
+//     togglePictureInPicture,
+//   } = usePictureInPicture(videoRef);
+
+//   return (
+//     <div className="App">
+//       <video ref={videoRef} id="pip-object" height={"400"}>
+//         <source src="vid-1.mp4" />
+//       </video>
+//       {isPictureInPictureAvailable && (
+//         <button
+//           onClick={() => togglePictureInPicture(!isPictureInPictureActive)
+
+//           }
+//         >
+//           {!isPictureInPictureActive ? "Open PIP" : "Close PIP"}
+//         </button>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useRef } from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
 function App() {
+  const videoRef = useRef(null);
+
+  const WindowContents = () => {
+    return (
+      <div className="App">
+        <h2>Document-picture-in-picture</h2>
+        <video
+          ref={videoRef}
+          controls
+          id="pip-object"
+          width={"500"}
+          height={"400"}
+        >
+          <source src="vid-2.mp4" />{" "}
+        </video>
+        <button onClick={() => window.documentPictureInPicture.window.close()}>
+          Close
+        </button>
+      </div>
+    );
+  };
+  const openWindow = async () => {
+    try {
+      const dpip = await window.documentPictureInPicture.requestWindow({
+        width: "500",
+        height: "500",
+      });
+      const pipDiv = dpip.document.createElement("div");
+      pipDiv.setAttribute("id", "pip-root");
+      dpip.document.body.append(pipDiv);
+      const pipRoot = ReactDOM.createRoot(
+        dpip.document.getElementById("pip-root")
+      );
+      pipRoot.render(<WindowContents />);
+    } catch (error) {
+      if (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openWindow}>Open DocumentPIP</button>
     </div>
   );
 }
